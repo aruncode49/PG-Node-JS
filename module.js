2d55a600e0af764a505c81ff57d72232854d892f
Arun Kumar <aruncode99@gmail.com>
2023-08-25 16:57:58 +0530

    initial commit with module learning

---
+++ module.js
@@ -0,0 +1,10 @@
+// const math = require("./math");
+const { add, sub } = require("./math");
+
+// console.log(math);
+
+// console.log(math.add(3, 5));
+// console.log(math.sub(5, 1));
+
+console.log(add(23, 21));
+console.log(sub(23, 21));
